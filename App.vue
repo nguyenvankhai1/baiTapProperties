2309a49d939a1ece28b6d971a19c97cab4bd74f4
nguyenvankhai1 <59248489+nguyenvankhai1@users.noreply.github.com>
2020-01-03 09:52:13 +0700

    baiTapProperties

---
+++ App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <hienThi/> -->
+    <!-- <VongDoi/> -->
+    <baiTapProperties postTitle='GIÁ TRỊ'></baiTapProperties>
+
+
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+// import hienThi from './components/hienThi'
+
+// import VongDoi from './components/VongDoi'
+
+import baiTapProperties from'./components/baiTapProperties'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+    // hienThi
+    // VongDoi
+    baiTapProperties
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ baiTapProperties.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="appp">
+       <div>{{postTitle}}</div>
+    </div>
+</template>
+<script>
+export default {
+    name:'appp',
+    props: ['postTitle'],
+    }
+
+</script>
+<style>
+
+</style>
